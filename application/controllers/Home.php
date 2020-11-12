9b7d9c4e814f0cb8a77829c5c4275931e8880be6
BuriedBullet <rubensherculano@hotmail.com>
2020-11-11 22:59:40 -0300

    Atualização

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,9 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
+    function __construct(){
+        parent:: __construct();
+        $this->load->model("Home_model", "m_home");
+    }
+
     public function index()
     {
-        
+        $data["aa"] = $this->m_home->test_type_return_get_filmes();
+        $this->load->view("template/header", $data, false);
+        $this->load->view("home/lista", $data, false);
+        $this->load->view("template/footer", $data, false);
     }
+
 
 }
